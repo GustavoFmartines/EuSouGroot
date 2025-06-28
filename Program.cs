6e63435155fe240790c481cead51a9e9083ee0b2
GustavoFmartines <gustavo.martines2003@outlook.com>
2025-06-28 04:03:03 -0300

    Foi criado um chat com o Groot!

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿    Console.Clear();
+
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+    Console.WriteLine("--- Chatbot com o Groot! ---");
+
+    Console.ResetColor();
+
+        string pergunta;
+
+do
+{
+    Console.Write("\nPergunta: ");
+    pergunta = Console.ReadLine()!;
+
+    if (pergunta.ToLower() == "tchau")
+    {
+        Console.WriteLine("\n         Resposta: Eu sou Groot!");
+    }
+    else
+    {
+        Console.WriteLine("\n         Resposta: Eu sou Groot.");
+    }
+
+} while (pergunta.ToLower() != "tchau");
